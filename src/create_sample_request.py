e20d8ab816ce0197c210e965ea372812321118fa
Ian P <pritchardians@gmail.com>
2024-06-16 08:49:33 -0500

    added first attribute to sample request

--- src/create_sample_request.py
+++ src/create_sample_request.py
@@ -3,7 +3,13 @@ from validate_json import validate_json
 # This ensures the sample request below is valid, and can be used when calling find_best_match
 validate_json("""
 {
-    "comparator": {},
+    "comparator": {
+        "attributes": {
+        "height": {
+                "perfect_score": 25,
+                "weight": 100            
+            }
+    },
     "candidates": {}
     
 }

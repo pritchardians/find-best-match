dbfdc2327d91588ec7061059faecb2b49f903156
Ian P <pritchardians@gmail.com>
2024-06-16 09:14:01 -0500

    added in more attributes to sample request

--- src/create_sample_request.py
+++ src/create_sample_request.py
@@ -1,14 +1,25 @@
 from validate_json import validate_json
 
-# This ensures the sample request below is valid, and can be used when calling find_best_match
+# The sample request below is valid, and can be used when calling find_best_match
+# NOTE: Shows that an attribute can have a missing perfect_score (defaults to 100)
+# NOTE: Shows that an attribute can have a missing weight (defaults to 100)
 validate_json("""
 {
     "comparator": {
         "attributes": {
-        "height": {
-                "perfect_score": 25,
+            "number_of_pages": {
+                "perfect_score": 250,
                 "weight": 100            
+            },
+            "humor_rating": {
+                "perfect_score": 57
+            },
+            "sentence_complexity": {
+                "weight": 100 
+            },
+            "child_friendliness": {
             }
+        }
     },
     "candidates": {}
     

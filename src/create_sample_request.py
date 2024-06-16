fdf0d2f7938c68c5bbaa062b3bbd2a646c41d40b
Ian P <pritchardians@gmail.com>
2024-06-16 10:02:19 -0500

    completed sample request json. Added ToDo to refactor it

--- src/create_sample_request.py
+++ src/create_sample_request.py
@@ -1,8 +1,11 @@
 from validate_json import validate_json
 
 # The sample request below is valid, and can be used when calling find_best_match
-# NOTE: Shows that an attribute can have a missing perfect_score (defaults to 100)
-# NOTE: Shows that an attribute can have a missing weight (defaults to 100)
+# NOTE: Shows that a comparator attribute can have a missing perfect_score (defaults to 100)
+# NOTE: Shows that a comparator  attribute can have a missing weight (defaults to 100)
+# NOTE: Shows that candidates can have missing attributes (default to a score of 0)
+# NOTE: Shows that candidates can have extra attributes - they will be ignored
+# ToDo: refactor this so the JSON is in a file in a sample json folder, read by the validator, run through auto-tests
 validate_json("""
 {
     "comparator": {
@@ -27,6 +30,19 @@ validate_json("""
                 "number_of_pages": 125,
                 "humor_rating": 12,
                 "child_friendliness": 73
+            },
+            "candidate_02": {
+                "attributes": {
+                    "number_of_pages": 47
+                }
+            },
+            "candidate_03": {
+                "attributes": {
+                    "number_of_pages": 125,
+                    "humor_rating": 12,
+                    "child_friendliness": 73,
+                    "not processed": 23
+                }
             }
         }
     }    

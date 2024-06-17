ee9ed08b95adcb994bb5c292489d48862e295dae
Ian P <pritchardians@gmail.com>
2024-06-17 07:21:17 -0500

    fixed issue in testing sample json request

--- sample_json/test_validate_sample_json.py
+++ sample_json/test_validate_sample_json.py
@@ -1,10 +1,18 @@
+import os
+import pytest
+
 from src.validate_json import validate_json
 
-sample_request = open("request.json")
+
+@pytest.fixture
+def sample_request():
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    sample_request_file = open(f"{this_dir}/request.json")
+    return sample_request_file.read()
 
 
-def test_validate_json():
-    assert validate_json (sample_request.read()) is True
+def test_validate_json(sample_request):
+    assert validate_json(sample_request) is True
 
 
 if __name__ == '__main__':

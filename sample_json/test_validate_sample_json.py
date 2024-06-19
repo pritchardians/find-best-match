1a6a134c7e367ab0b3847da89ce4a256162449f2
Ian P <pritchardians@gmail.com>
2024-06-19 17:16:56 -0500

    refactored test_validate_sample_json to use a read_file function

--- sample_json/test_validate_sample_json.py
+++ sample_json/test_validate_sample_json.py
@@ -3,6 +3,7 @@ import pytest
 
 from src.validate_json import validate_json
 
+
 # ToDo: Refactor me! This uses 2 similar fixtures and functions. Feed the sample name into 1 function?
 # ToDo: Can fixtures have inputs? Or convert to standard functions?
 
@@ -13,19 +14,24 @@ def sample_request():
     return sample_request_file.read()
 
 
-@pytest.fixture
-def sample_response():
+def read_file(filename: str) -> str:
+    """
+    Open a file and return its contents as a string. Use a relative filepath
+    :param filename: filename to open
+    :return: the data in the file
+    :rtype: str
+    """
     this_dir = os.path.dirname(os.path.realpath(__file__))
-    sample_request_file = open(f"{this_dir}/response.json")
+    sample_request_file = open(f"{this_dir}/{filename}")
     return sample_request_file.read()
 
 
-def test_validate_json_request(sample_request):
-    assert validate_json(sample_request) is True
+def test_validate_json_request():
+    assert validate_json(read_file("request.json")) is True
 
 
-def test_validate_json_response(sample_response):
-    assert validate_json(sample_response) is True
+def test_validate_json_response():
+    assert validate_json(read_file("response.json")) is True
 
 
 if __name__ == '__main__':

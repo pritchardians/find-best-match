759315e0a0538a0f4960231f1ed679af2ee5c20a
Ian P <pritchardians@gmail.com>
2024-06-21 08:01:37 -0500

    get-project-root utility added. Needs auto-testing!

---
+++ src/utils/get_project_root.py
@@ -0,0 +1,26 @@
+import os
+import logging
+
+
+def get_project_root(project_name: str) -> str:
+    """
+    Get the path of the project root, to use in relative path processing
+    :param project_name: Name of the current project
+    :return: Project root path
+    :rtype: str
+    """
+    # Create a list of the path nodes i the current working directory
+    full_path_list: list[str] = os.getcwd().split(os.sep)
+    # Get all the path nodes from the list up to the project path, and join is by the os. sep
+    # (probably \ or / depending on whether you are running on Unix or Windows)
+    try:
+        return os.sep.join(os.getcwd().split(os.sep)[:full_path_list.index(project_name) + 1:])
+    except ValueError:
+        err_mess: str = "Incorrect project name sent to get_project_root function. "
+        err_mess += f"Argument given was {project_name}"
+        logging.error(err_mess)
+        raise ValueError(err_mess)
+
+
+if __name__ == '__main__':
+    print(get_project_root("find-best-matchy"))
